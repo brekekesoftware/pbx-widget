20c29bf7dd29745b9ca2fa43f383736b8161c8c7
Esi're <esiri92@gmail.com>
2023-05-18 13:45:21 +0000

    chore: build salesforce index.html.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,33 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  build: {
+    // cssCodeSplit: false,
+    // manifest: true,
+    // sourcemap: true,
+    // assetsInlineLimit: 0,
+    // lib: {
+    //   // name: 'brekekewidget',
+    //   entry: {
+    //     widget: './src/main.tsx',
+    //     // main: './index.html',
+    //     // salesforce: './salesforce/index.html',
+    //   },
+    //   formats: ['es'],
+    //   fileName: (format, entryName) => {
+    //     return `${entryName}/index.js`;
+    //   },
+    // },
+
+    rollupOptions: {
+      input: {
+        widget: './src/main.tsx',
+        // main: './index.html',
+        salesforce: './salesforce/index.html',
+      },
+      // external: ['react', 'react-dom']
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
