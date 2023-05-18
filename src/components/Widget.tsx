f0b6d7d477f9f03074188f629e4e646b762e764f
Esi're <esiri92@gmail.com>
2023-05-18 13:41:26 +0000

    chore: cleanup Widget.tsx.

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,10 +1,9 @@
 import Auth from '@/components/Auth';
 import Main from '@/components/Main';
-import '@/widget.css';
 import { authState } from '@/state/authState';
+import '@/widget.css';
 import { configure } from 'mobx';
 import { useObserver } from 'mobx-react';
-import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
 
 configure({ isolateGlobalState: true });
@@ -12,12 +11,19 @@ configure({ isolateGlobalState: true });
 const Widget = () => {
   const loggedIn = useObserver(() => authState.loggedIn);
 
+  const renderView = () => {
+    if (!loggedIn) {
+      return <Auth />;
+    }
+
+    return <Main />;
+  }
+
   return (
     <RecoilRoot>
       <div className='w-widget border rounded shadow m-2'>
         <div id="webphone_embed" style={{ display: 'none' }} />
-        {!loggedIn && <Auth />}
-        {loggedIn && <Main />}
+        {renderView()}
       </div>
     </RecoilRoot>
   );
