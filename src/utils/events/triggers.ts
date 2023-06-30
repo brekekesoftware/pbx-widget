242ede72064b284c095c15c3c8ee8b56f5d81e8c
Esi're <esiri92@gmail.com>
2023-06-30 19:17:52 +0000

    chore: cleanup.

--- src/utils/events/triggers.ts
+++ src/utils/events/triggers.ts
@@ -1,5 +1,4 @@
-import { CallInfo, GlobalEventTrigger, Log } from '@core/types/events';
-import { Call } from '@core/types/phone';
+import { GlobalEventTrigger } from '@core/types/events';
 import { fireEvent } from '@core/utils/events/common';
 
 export const fireCallEvent: GlobalEventTrigger<'call'> = (call: any) => {
@@ -10,16 +9,16 @@ export const fireMakeCallEvent: GlobalEventTrigger<'make-call'> = (number: strin
   return fireEvent('make-call', { number });
 }
 
-export const fireNumberEntryEvent: GlobalEventTrigger<'number-entry'> = (number: string) => fireEvent('number-entry', { number });
+export const fireNumberEntryEvent: GlobalEventTrigger<'number-entry'> = (number) => fireEvent('number-entry', { number });
 
-export const fireCallUpdatedEvent: GlobalEventTrigger<'call-updated'> = (call: any) => fireEvent('call-updated', { call });
-export const fireCallEndedEvent: GlobalEventTrigger<'call-ended'> = (call: any) => fireEvent('call-ended', { call });
+export const fireCallUpdatedEvent: GlobalEventTrigger<'call-updated'> = (call) => fireEvent('call-updated', { call });
+export const fireCallEndedEvent: GlobalEventTrigger<'call-ended'> = (call) => fireEvent('call-ended', { call });
 
 export const fireLoggedInEvent: GlobalEventTrigger<'logged-in'> = (account) => fireEvent('logged-in', { account });
 export const fireLoggedOutEvent: GlobalEventTrigger<'logged-out'> = () => fireEvent('logged-out');
 
-export const fireCallInfoEvent: GlobalEventTrigger<'call-info'> = (call: Call, info: CallInfo) => fireEvent('call-info', { call, info });
+export const fireCallInfoEvent: GlobalEventTrigger<'call-info'> = (call, info) => fireEvent('call-info', { call, info });
 
-export const fireLogEvent: GlobalEventTrigger<'log'> = (log: Log) => fireEvent('log', { log });
-export const fireLogSavedEvent: GlobalEventTrigger<'log-saved'> = (log: Log) => fireEvent('log-saved', { log });
+export const fireLogEvent: GlobalEventTrigger<'log'> = (log) => fireEvent('log', { log });
+export const fireLogSavedEvent: GlobalEventTrigger<'log-saved'> = (log) => fireEvent('log-saved', { log });
 
