4f1757ed32d40f1569c885eda90331f0072fb99f
Esi're <esiri92@gmail.com>
2023-06-30 19:27:39 +0000

    chore: remove unused imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import viteLogo from '/vite.svg';
 import Widget from '@/components/Widget';
-import { useState } from 'react';
-// import './App.css';
-import reactLogo from './assets/react.svg';
 
 function App() {
   const env = import.meta.env;
