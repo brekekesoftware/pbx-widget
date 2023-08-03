ef9fd174179824cd130e4e716b24ebdfdffb6764
Esi're <esiri92@gmail.com>
2023-08-03 17:30:46 +0000

    fix: call logs with duplicate contacts now correctly show selected contact.

--- src/components/CallLog.tsx
+++ src/components/CallLog.tsx
@@ -9,16 +9,34 @@ import { Call } from '@/types/phone';
 import { c } from '@/utils/html-class';
 import { PhoneIcon } from '@heroicons/react/24/solid';
 import { useObserver } from 'mobx-react';
+import { FC } from 'react';
 
 const CallLog = () => {
   const calls = useObserver(() => callsState.inactiveCalls);
+
+  return (
+    <div className='overflow-auto mb-12'>
+      <p className='p-2 font-bold text-sm border-b sticky top-0 bg-gray-100'>
+        Calls ({calls.length})
+      </p>
+      <div className='divide-y'>
+        {calls.map(call => <EndedCall call={call} />)}
+      </div>
+    </div>
+  );
+}
+
+interface EndedCallProps {
+  call: Call;
+}
+
+const EndedCall: FC<EndedCallProps> = ({ call }) => {
   const logEnabled = useObserver(() => configState.logEnabled);
   const logButtonTitle = useObserver(() => configState.logButtonTitle);
 
-  const displayNames = useObserver(() => calls.reduce((acc, call) => {
-    acc[call.pbxRoomId] = callsState.displayName(call);
-    return acc;
-  }, {} as Record<string, string | undefined>));
+  const displayName = useObserver(() => callsState.displayName(call));
+  const hasMultipleContacts = useObserver(() => callsState.callHasMultipleContacts(call));
+  const saved = useObserver(() => logState.callLogSaved(call));
 
   const renderDuration = (call: Call) => {
     if (!call.answered) return null;
@@ -30,46 +48,29 @@ const CallLog = () => {
     );
   }
 
-  const renderCall = (call: Call) => {
-    const displayName = displayNames[call.pbxRoomId];
-    const hasMultipleContacts = callsState.callHasMultipleContacts(call);
-    const saved = logState.callLogSaved(call);
-
-    return (
-      <div key={call.id} className='bg-white p-1.5 flex justify-between'>
-        <div>
-          <CallContacts call={call} disabled={!hasMultipleContacts || saved}>
-            {({ open }) => (
-              <p className={c('font-bold text-sm', { 'transition-all cursor-pointer rounded hover:bg-app/20 hover:p-1': hasMultipleContacts && !saved, 'rounded bg-app/20 p-1': open })}>
-                {displayName ?? call.getDisplayName()}
-                {displayName && <span className='font-normal text-xs ml-2'>({call.partyNumber})</span>}
-              </p>
-            )}
-          </CallContacts>
-          <span className="text-xs">{callStatus(call)}</span>
-          {renderDuration(call)}
-        </div>
-        <div className="flex items-center gap-2.5">
-          <button className='h-6 w-6 p-1 rounded-full' onClick={() => pbx.call(call.partyNumber)} title='Dial'>
-            <PhoneIcon className='text-green-400 h-4 w-4' />
-          </button>
-          {logEnabled && (
-            <button onClick={() => logState.open(call)} title={logButtonTitle}>
-              <img className='h-6 w-6' src={NoteIcon} alt={logButtonTitle} />
-            </button>
-          )}
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className='overflow-auto mb-12'>
-      <p className='p-2 font-bold text-sm border-b sticky top-0 bg-gray-100'>
-        Calls ({calls.length})
-      </p>
-      <div className='divide-y'>
-        {calls.map(renderCall)}
+    <div key={call.id} className='bg-white p-1.5 flex justify-between'>
+      <div>
+        <CallContacts call={call} disabled={!hasMultipleContacts || saved}>
+          {({ open }) => (
+            <p className={c('font-bold text-sm', { 'transition-all cursor-pointer rounded hover:bg-app/20 hover:p-1': hasMultipleContacts && !saved, 'rounded bg-app/20 p-1': open })}>
+              {displayName ?? call.getDisplayName()}
+              {displayName && <span className='font-normal text-xs ml-2'>({call.partyNumber})</span>}
+            </p>
+          )}
+        </CallContacts>
+        <span className="text-xs">{callStatus(call)}</span>
+        {renderDuration(call)}
+      </div>
+      <div className="flex items-center gap-2.5">
+        <button className='h-6 w-6 p-1 rounded-full' onClick={() => pbx.call(call.partyNumber)} title='Dial'>
+          <PhoneIcon className='text-green-400 h-4 w-4' />
+        </button>
+        {logEnabled && (
+          <button onClick={() => logState.open(call)} title={logButtonTitle}>
+            <img className='h-6 w-6' src={NoteIcon} alt={logButtonTitle} />
+          </button>
+        )}
       </div>
     </div>
   );
