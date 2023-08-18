6dd555e08f6b0f81b12536e3916436157846c23d
Esi're <esiri92@gmail.com>
2023-08-18 02:49:42 +0000

    chore: added SVGIcon component, check note before use.

---
+++ src/components/SVGIcon.tsx
@@ -0,0 +1,60 @@
+import { logger } from '@/utils/logger';
+import { FC, SVGProps, useEffect, useRef, useState } from 'react';
+
+type SVGComponent = FC<SVGProps<SVGSVGElement>>;
+
+export interface SVGIconOptions {
+  onCompleted?: (name: Icons, svg: SVGComponent | undefined) => void;
+  onError?: (error: Error) => void;
+}
+
+export const useSVGIcon = (name: Icons, options: SVGIconOptions = {}) => {
+  const iconRef = useRef<SVGComponent>();
+  const [error, setError] = useState<Error>();
+  const [loading, setLoading] = useState(false);
+
+  const loadIcon = async () => {
+    setLoading(true);
+    try {
+      const module = await import(`@/assets/icons/${name}.svg`);
+      iconRef.current = module.ReactComponent;
+      options.onCompleted?.(name, iconRef.current);
+      logger('SVGIcon', `Loaded icon: ${name}`, module);
+    } catch (error) {
+      setError(error as Error);
+      options.onError?.(error as Error);
+      logger('SVGIcon', `Failed to load icon: ${name}`, error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => void loadIcon(), [name]);
+
+  return { error, loading, Icon: iconRef.current };
+};
+
+interface Props extends SVGProps<SVGSVGElement> {
+  name: Icons;
+}
+
+// NOTE: seems to split svgs into separate chunks from the main bundle
+const SVGIcon: FC<Props> = ({ name, ...svgProps }) => {
+  const { Icon } = useSVGIcon(name);
+
+  return Icon ? <Icon {...svgProps} /> : null;
+};
+
+export default SVGIcon;
+
+type Icons =
+  'attended-transfer'
+  | 'blind-transfer'
+  | 'keypad'
+  | 'mic'
+  | 'mic-off'
+  | 'note'
+  | 'note-submitted'
+  | 'pause'
+  | 'play'
+  | 'user';
